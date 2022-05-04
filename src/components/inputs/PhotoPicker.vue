b9bc33930d2079c4ba9231bb9c327a8390e4bb62
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-05-04 14:07:03 -0300

    Ajustado PhotoPicker para ver pela utrl também.

--- src/components/inputs/PhotoPicker.vue
+++ src/components/inputs/PhotoPicker.vue
@@ -1,92 +1,107 @@
 <script setup>
-    const props = defineProps({
-        label: {
-            type: String,
-            default: "",
-            required: true
-        },
-        modelValue: {
-            type: [String, Number, Boolean, Object, Array, File],
-            default: "",
-            required: true
-        },
-        id: {
-            type: String,
-            default: "text",
-            required: true
-        },
-        text: {
-            type: String,
-            default: "",
-            required: true
-        },
-        required: {
-            type: Boolean,
-            default: false
-        }
-    });
-    const emits = defineEmits(["update:modelValue"]);
+import { computed } from "vue";
 
-    let base64 = null;
-    let imageSrc = null;
-
-    function removeImage() {
-        base64 = null;
-        imageSrc = null;
-        emits("update:modelValue", null);
+const props = defineProps({
+    label: {
+        type: String,
+        default: "",
+        required: true
+    },
+    modelValue: {
+        type: [String, Number, Boolean, Object, Array, File],
+        default: "",
+        required: true
+    },
+    id: {
+        type: String,
+        default: "text",
+        required: true
+    },
+    text: {
+        type: String,
+        default: "",
+        required: true
+    },
+    required: {
+        type: Boolean,
+        default: false
     }
+});
+const emits = defineEmits(["update:modelValue"]);
 
-    function urlToBase64(urlStorage) {
-        function toDataURL(url, callback) {
-            const xhr = new XMLHttpRequest();
-            xhr.onload = function () {
-                const reader = new FileReader();
-                reader.onloadend = function () {
-                    callback(reader.result);
-                };
-                reader.readAsDataURL(xhr.response);
+let base64 = null;
+let imageSrc = null;
+
+function removeImage() {
+    base64 = null;
+    imageSrc = null;
+    emits("update:modelValue", null);
+}
+
+function urlToBase64(urlStorage) {
+    function toDataURL(url, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            const reader = new FileReader();
+            reader.onloadend = function () {
+                callback(reader.result);
             };
-            xhr.open("GET", url);
-            xhr.responseType = "blob";
-            xhr.send();
-        }
-
-        toDataURL(urlStorage, function (dataUrl) {
-            base64 = dataUrl;
-            emits("update:value", {
-                base64: dataUrl,
-                name: null,
-                format: "png"
-            });
-        });
-    }
-
-    function uploadImage(event) {
-        const reader = new FileReader();
-        reader.readAsDataURL(event.target.files[0]);
-        reader.onload = () => {
-            imageSrc = event.target.files[0];
-            base64 = reader.result;
-            emits("update:modelValue", {
-                base64: reader.result,
-                name: imageSrc.name,
-                format: imageSrc.type.replace("image/", "")
-            });
+            reader.readAsDataURL(xhr.response);
         };
+        xhr.open("GET", url);
+        xhr.responseType = "blob";
+        xhr.send();
     }
+
+    toDataURL(urlStorage, function (dataUrl) {
+        base64 = dataUrl;
+        emits("update:value", {
+            base64: dataUrl,
+            name: null,
+            format: "png"
+        });
+    });
+}
+
+function uploadImage(event) {
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    reader.onload = () => {
+        imageSrc = event.target.files[0];
+        base64 = reader.result;
+        emits("update:modelValue", {
+            base64: reader.result,
+            name: imageSrc.name,
+            format: imageSrc.type.replace("image/", "")
+        });
+    };
+}
+
+const imageSource = computed(() => {
+    return props.modelValue.includes("http")
+        ? props.modelValue
+        : props.modelValue.base64;
+});
 </script>
 
 <template>
     <label class="block text-sm font-medium text-gray-700">{{ label }}:</label>
     <div class="mt-4 flex items-center">
         <img
-            :src="modelValue.base64"
+            :src="imageSource"
             v-if="modelValue"
             class="inline-block h-16 w-16 rounded-full overflow-hidden"
             alt="" />
         <span
             v-else
-            class="inline-block h-16 w-16 rounded-full overflow-hidden bg-gray-100">
+            class="
+                inline-block
+                h-16
+                w-16
+                rounded-full
+                overflow-hidden
+                bg-gray-100
+            ">
             <svg
                 class="h-full w-full text-gray-300"
                 fill="currentColor"
@@ -99,7 +114,35 @@
             type="file"
             accept="image/*"
             @change="uploadImage"
-            class="ml-5 bg-white w-4/5 py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100" />
+            class="
+                ml-5
+                bg-white
+                w-4/5
+                py-2
+                px-3
+                border border-gray-300
+                rounded-md
+                shadow-sm
+                text-sm
+                leading-4
+                font-medium
+                text-gray-700
+                hover:bg-gray-50
+                focus:outline-none
+                focus:ring-2
+                focus:ring-offset-2
+                focus:ring-indigo-500
+                file:mr-4
+                file:py-2
+                file:px-4
+                file:rounded-full
+                file:border-0
+                file:text-sm
+                file:font-semibold
+                file:bg-indigo-50
+                file:text-indigo-700
+                hover:file:bg-indigo-100
+            " />
         <svg
             v-if="base64"
             xmlns="http://www.w3.org/2000/svg"
