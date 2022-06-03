7b4029ec517848cdc8a4ef30932483620e0090ab
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-06-03 17:38:59 -0300

    Tooltip pros botões

--- src/views/home/Chat.vue
+++ src/views/home/Chat.vue
@@ -103,7 +103,7 @@ Socket.on("returnedToBot", async () => {
 						w-12
 						h-12
 					"
-					placeholder="Retornar conversa para o robô"
+					title="Retornar conversa para o robô"
 					@click="returnToBot(false)">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -131,7 +131,7 @@ Socket.on("returnedToBot", async () => {
 						w-12
 						h-12
 					"
-					placeholder="Assumir conversa"
+					title="Assumir conversa"
 					@click="returnToBot(true)">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 						<path
@@ -161,6 +161,7 @@ Socket.on("returnedToBot", async () => {
 						w-12
 						h-12
 					"
+					title="Enviar mensagem"
 					@click="sendMessage()">
 					<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
 						<path
