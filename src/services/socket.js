b77af49cdd8beb1a68baa9046dca8b3c8faff22f
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-06-23 16:46:07 -0300

    Socket address

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3005");
+const socket = io("https://napi.aquicob.com.br:3006/");
 
 socket.on("connect_error", (err) => {
     console.log(`connect_error due to ${err.message}`);
