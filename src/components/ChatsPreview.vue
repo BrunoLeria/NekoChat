172da5e9d74a4a4a4a06218d08f335ea39042db8
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2022-06-24 17:17:24 -0300

    Receber imagem e video

--- src/components/ChatsPreview.vue
+++ src/components/ChatsPreview.vue
@@ -13,6 +13,28 @@ const time = (message) => {
 	return date.getMinutes() < 10 ? date.getHours() + ":0" + date.getMinutes() : date.getHours() + ":" + date.getMinutes();
 };
 
+function trataMensagem(message) {
+	if (message.includes("IMAGEM")) {
+		return (
+			"{Imagem disponível na conversa completa} \n" +
+			message.replace(/IMAGEM:(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]) TEXTO: /, "")
+		);
+	}
+	if (message.includes("DOCUMENTO")) {
+		return (
+			"{Documento disponível na conversa completa} \n" +
+			message.replace(/DOCUMENTO:(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]) NOME: /, "")
+		);
+	}
+	if (message.includes("VIDEO")) {
+		return (
+			"{Vídeo disponível na conversa completa} \n" +
+			message.replace(/VIDEO:(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]) TEXTO: /, "")
+		);
+	}
+	return message;
+}
+
 const messages = computed(() => {
 	if (props.talk[1] && props.talk[0].tlk_date_time > props.talk[1].tlk_date_time) return props.talk.reverse();
 	return props.talk;
@@ -39,7 +61,7 @@ const messages = computed(() => {
 				</p>
 			</div>
 			<p class="my-3 break-all">
-				{{ message.tlk_message }}
+				{{ trataMensagem(message.tlk_message) }}
 			</p>
 		</div>
 	</div>
--- src/views/home/Chat.vue
+++ src/views/home/Chat.vue
@@ -43,6 +43,21 @@ function sendMessage() {
 	}
 }
 
+function getSource(message) {
+	const source = message.match(/(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/)[0].replace(" ", "");
+	return source;
+}
+
+function getText(message) {
+	if (message.includes("IMAGEM")) {
+		return message.replace(/IMAGEM:(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]) TEXTO: /, "");
+	}
+	if (message.includes("DOCUMENTO")) {
+		return message.replace(/DOCUMENTO:(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]) NOME: /, "");
+	}
+	return message.replace(/VIDEO:(http|ftp| https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]) TEXTO: /, "");
+}
+
 //WebSocket chenges to the dashboard and the selected talk
 Socket.on("returnedToBot", async () => {
 	await talkStore.findOneTalkByChatID().then(() => {
@@ -81,7 +96,28 @@ watch(
 						{{ time(message) }}
 					</p>
 				</div>
-				<p class="my-3 break-all">
+				<div v-if="message.tlk_message.includes('IMAGEM: ')">
+					<img :src="getSource(message.tlk_message)" class="w-full h-full max-w-xs max-h-60" />
+					<p v-if="getText(message.tlk_message).length > 0" class="my-3 break-all">
+						{{ getText(message.tlk_message) }}
+					</p>
+				</div>
+				<div v-else-if="message.tlk_message.includes('DOCUMENTO: ')">
+					<a class="my-3 break-all" :href="getSource(message.tlk_message)">
+						{{ "DOCUMENTO: " + getText(message.tlk_message) }}
+					</a>
+				</div>
+				<div v-else-if="message.tlk_message.includes('VIDEO: ')">
+					<video class="w-full h-full max-w-xs max-h-60" controls>
+						<source :src="getSource(message.tlk_message)" type="video/mp4" />
+						<source :src="getSource(message.tlk_message)" type="video/ogg" />
+						Your browser does not support the video tag.
+					</video>
+					<p v-if="getText(message.tlk_message).length > 0" class="my-3 break-all">
+						{{ getText(message.tlk_message) }}
+					</p>
+				</div>
+				<p v-else class="my-3 break-all">
 					{{ message.tlk_message }}
 				</p>
 			</div>
