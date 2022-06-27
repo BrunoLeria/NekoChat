bff741c589c925ee49984c78df663002fcaba1f4
Bruno Aquicob <bruno.acerteaqui@gmail.com>
2022-06-27 01:57:36 -0300

    Fixed size para icone de avatar. Depois verificar como fazer isso de maneira dinâmica.

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -76,9 +76,9 @@ function configLoggedUser() {
 						<img
 							v-if="userStore.user.usu_photo"
 							:src="userStore.user.usu_photo"
-							class="inline-block h-10 rounded-full overflow-hidden"
+							class="inline-block h-12 w-12 rounded-full overflow-hidden"
 							alt="" />
-						<span v-else class="inline-block h-10 w-10 rounded-full overflow-hidden bg-gray-100">
+						<span v-else class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
 							<svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
 								<path
 									d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
