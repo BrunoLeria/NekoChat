92123c3811c4c5d7c025a00a27f736362e375b0b
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-06-29 15:27:15 -0300

    Error notification

---
+++ src/components/NotificationHandler.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, defineProps } from "vue";
+
+const props = defineProps({
+	message: String
+});
+
+const isOpen = ref(false);
+</script>
+<template>
+	<teleport to="body">
+		<div class="model">
+			<div>
+				<h2>Atenção:</h2>
+				<p>{{ props.message }}</p>
+				<button @click="">Fechar</button>
+			</div>
+		</div>
+	</teleport>
+</template>
+<style>
+.modal {
+	position: absolute;
+	top: 0;
+	left: 0;
+	background-color: 0, 0, 0, 0.1;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.modal > div {
+	background-color: white;
+	padding: 50px;
+	border-radius: 10px;
+}
+</style>
