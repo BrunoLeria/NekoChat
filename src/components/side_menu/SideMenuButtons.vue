8d7e69c53271226fc577766d18fc82cfe288246b
BrunoAquiCob <bruno.acerteaqui@gmail.com>
2022-08-05 18:05:49 -0300

    Quick ifx on overflowing text on buttons

--- src/components/side_menu/SideMenuButtons.vue
+++ src/components/side_menu/SideMenuButtons.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { signOut } from "firebase/auth";
+import { signOut, getAuth } from "firebase/auth";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useUsersStore } from "/src/services/stores/users";
-import { getAuth } from "firebase/auth";
 
 const auth = getAuth();
 const userStore = useUsersStore();
 const router = useRouter();
 const emit = defineEmits(["update:modelValue"]);
+const showLoggout = ref(false);
+const showConfig = ref(false);
+const showTeam = ref(false);
 
 const handleSignOut = () => {
 	signOut(auth).then(() => {
@@ -30,17 +33,27 @@ function updateStatus(sts_identification) {
 }
 </script>
 <template>
-	<div class="flex">
-		<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-xl ease-in-out duration-500" @click="handleSignOut">
+	<div class="flex justify-between">
+		<div
+			class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
+			@mouseover="showLoggout = true"
+			@mouseleave="showLoggout = false"
+			@click="handleSignOut">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 				<path
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
 			</svg>
-			<h4 class="mx-3">Sair</h4>
+			<transition name="slide-fade" :duration="{ enter: 50, leave: 50 }">
+				<h4 v-show="showLoggout" class="mx-3 sm:hidden lg:inline">Sair</h4>
+			</transition>
 		</div>
-		<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-xl ease-in-out duration-500" @click="configLoggedUser">
+		<div
+			class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
+			@mouseover="showConfig = true"
+			@mouseleave="showConfig = false"
+			@click="configLoggedUser">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 				<path
 					stroke-linecap="round"
@@ -48,10 +61,14 @@ function updateStatus(sts_identification) {
 					d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
 				<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 			</svg>
-			<h4 class="mx-3">Configurações</h4>
+			<transition name="slide-fade" :duration="{ enter: 50, leave: 50 }">
+				<h4 v-show="showConfig" class="mx-3 sm:hidden lg:inline">Configurações</h4>
+			</transition>
 		</div>
 		<div
-			class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-xl ease-in-out duration-500"
+			class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
+			@mouseover="showTeam = true"
+			@mouseleave="showTeam = false"
 			@click="$emit('update:modelValue', 'Team')">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 				<path
@@ -59,7 +76,9 @@ function updateStatus(sts_identification) {
 					stroke-linejoin="round"
 					d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
 			</svg>
-			<h4 class="mx-3">Equipe</h4>
+			<transition name="slide-fade" :duration="{ enter: 50, leave: 50 }">
+				<h4 v-show="showTeam" class="mx-3 sm:hidden lg:inline">Equipe</h4>
+			</transition>
 		</div>
 	</div>
 </template>
