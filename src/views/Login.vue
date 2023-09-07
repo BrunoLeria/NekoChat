cb8647cf3a04db6fafbf39f772adcaa13a77a4fc
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-07 16:36:58 -0300

    Update logo image source in Login.vue to use nekologo.jpeg and add rounded corners

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,7 @@
 import TextInput from "/src/components/inputs/TextInput.vue";
 import Sublink from "/src/components/buttons/Sublink.vue";
 import PasswordInput from "../components/inputs/PasswordInput.vue";
-import logo from "/src/assets/logo.svg";
+import logo from "/src/assets/nekologo.jpeg";
 import { ref, onBeforeMount, onMounted } from "vue";
 import { useUsersStore } from "/src/services/stores/users.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from "firebase/auth";
@@ -11,7 +11,6 @@ import { useRouter } from "vue-router";
 const email = ref("");
 const password = ref("");
 const errMsg = ref("");
-const remember = false;
 const router = useRouter();
 const userStore = useUsersStore();
 
@@ -95,7 +94,7 @@ async function login(data) {
 	<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" @keypress.enter="signInWithEmail()">
 		<div class="max-w-md w-full space-y-8">
 			<div>
-				<img class="mx-auto h-36 w-auto" :src="logo" alt="Workflow" />
+				<img class="mx-auto h-36 w-auto rounded-full" :src="logo" alt="Workflow" />
 				<h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Entre com a sua conta</h2>
 				<p class="mt-2 text-center text-sm text-gray-600">
 					Ou
@@ -109,21 +108,14 @@ async function login(data) {
 						{{ errMsg }}
 					</p>
 					<TextInput label="Email" v-model="email" type="email" id="email" autoComplete="email" />
-					<PasswordInput
-						label="Senha"
-						v-model="password"
-						type="password"
-						id="password"
-						autoComplete="current-password"
-						:password-score-show="false" />
+					<PasswordInput label="Senha" v-model="password" type="password" id="password"
+						autoComplete="current-password" :password-score-show="false" />
 					<div class="flex items-center justify-center">
 						<Sublink text="Esqueceu a sua senha?" route="ForgotPassword" />
 					</div>
 				</div>
 				<div>
-					<button
-						type="button"
-						class="
+					<button type="button" class="
 							group
 							relative
 							w-full
@@ -141,18 +133,13 @@ async function login(data) {
 							focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
 							ease-in-out
 							duration-500
-						"
-						@click="signInWithEmail">
+						" @click="signInWithEmail">
 						<span class="absolute left-0 inset-y-0 flex items-center pl-3">
 							<!-- Heroicon name: solid/lock-closed -->
-							<svg
-								class="lock-closed h-5 w-5 text-blue-500 group-hover:text-blue-400"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 20 20"
-								fill="currentColor"
+							<svg class="lock-closed h-5 w-5 text-blue-500 group-hover:text-blue-400"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
 								aria-hidden="true">
-								<path
-									fill-rule="evenodd"
+								<path fill-rule="evenodd"
 									d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
 									clip-rule="evenodd" />
 							</svg>
