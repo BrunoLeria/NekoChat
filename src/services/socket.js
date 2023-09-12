efc2da41032f9a117258b564e908c187cfcd1273
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-12 20:38:25 -0300

    Update socket connection port to 3005

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:80/");
+const socket = io("http://localhost:3005/");
 
 socket.on("connect_error", (err) => {
     console.log(`connect_error due to ${err.message}`);
