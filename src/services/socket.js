cc3803c7a7b2f687c8d1e1bb5d4c574437a6f708
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-24 20:31:41 -0300

    Refactor socket connection to use the configured URL

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+import { GCurl } from "../config/url";
 
-const socket = io("http://localhost:3005/");
+const socket = io(GCurl);
 
 socket.on("connect_error", (err) => {
     console.log(`connect_error due to ${err.message}`);
