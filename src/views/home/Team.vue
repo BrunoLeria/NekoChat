547021822973dfe177da292520d5ba2602a3f527
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-24 20:47:03 -0300

    Refactor team member status and name retrieval logic in Team.vue

--- src/views/home/Team.vue
+++ src/views/home/Team.vue
@@ -1,26 +1,21 @@
 <script setup>
 import { useTeamStore } from "../../services/stores/team";
 import { useUsersStore } from "../../services/stores/users";
-import { ref, onMounted, computed } from "vue";
+import { useStatusesStore } from "../../services/stores/status";
 
 const teamStore = useTeamStore();
 const userStore = useUsersStore();
 const emit = defineEmits(["update:modelValue"]);
-const statuses = [
-	{ sts_identification: 1, sts_name: "Online", sts_description: "Disponível", sts_color: "rgb(74 222 128)" },
-	{ sts_identification: 2, sts_name: "Occupied", sts_description: "Ocupado", sts_color: "rgb(248 113 113)" },
-	{ sts_identification: 3, sts_name: "Away", sts_description: "Ausente", sts_color: "rgb(250 204 21)" },
-	{ sts_identification: 4, sts_name: "Offline", sts_description: "Desconectado", sts_color: "rgb(156 163 175)" }
-];
+const statuses = useStatusesStore().statuses;
 
 teamStore.findAllTeam();
 
 function statusColor(id) {
-	return statuses[id - 1].sts_color;
+	return statuses[id - 1].color;
 }
 
-function officeName(id) {
-	return userStore.offices[id - 1].name;
+function teamName(id) {
+	return teamStore.teams[id - 1];
 }
 
 function openUserConfig(id) {
@@ -44,22 +39,22 @@ function openUserConfig(id) {
 				ease-in-out
 				duration-500
 			"
-			:key="member.usu_identification">
+			:key="member.identification">
 			<div class="flex p-3 justify-between">
 				<div class="flex flex-col p-3">
 					<div class="flex">
-						<h4 class="font-semibold text-lg">{{ member.usu_name ? member.usu_name : "Usuário " + member.usu_identification }}</h4>
-						<p v-if="member.usu_is_admin" class="mx-3 bg-green-100 text-emerald-700 rounded-full px-2 font-semibold text-base h-6 w-16">
+						<h4 class="font-semibold text-lg">{{ member.name ? member.name : "Usuário " + member.identification }}</h4>
+						<p v-if="member.is_admin" class="mx-3 bg-green-100 text-emerald-700 rounded-full px-2 font-semibold text-base h-6 w-16">
 							Admin
 						</p>
 					</div>
-					<p class="text-gray-500">{{ officeName(member.usu_fk_ofc_identification) }}</p>
+				<p class="text-gray-500">{{ teamName(member.fk_team_identification) }}</p>
 				</div>
 				<div class="inline-block relative">
 					<img
-						v-if="member.usu_photo"
-						:src="member.usu_photo"
-						:alt="member.usu_name + ' avatar'"
+						v-if="member.photo"
+						:src="member.photo"
+						:alt="member.name + ' avatar'"
 						class="rounded-full h-12 w-12 border-2"
 						referrerpolicy="no-referrer" />
 					<span v-else class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
@@ -69,13 +64,13 @@ function openUserConfig(id) {
 						</svg>
 					</span>
 					<div
-						:style="{ backgroundColor: statusColor(member.usu_fk_sts_identification) }"
+						:style="{ backgroundColor: statusColor(member.fk_statuses_identification) }"
 						class="absolute top-8 left-8 z-10 p-2 rounded-full"></div>
 				</div>
 			</div>
 			<div class="flex">
 				<button
-					v-if="userStore.user.usu_is_admin"
+					v-if="userStore.user.is_admin"
 					class="
 						w-full
 						flex
@@ -88,7 +83,7 @@ function openUserConfig(id) {
 						ease-in-out
 						duration-500
 					"
-					@click="openUserConfig(member.usu_identification)">
+					@click="openUserConfig(member.identification)">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						class="h-6 w-6 mx-2"
