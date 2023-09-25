23d2a94e83ec7de784c9fe0ae8e4406a57e7689c
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-25 02:56:21 -0300

    Fix router push for error handling on "talks" store Remove updateRobot, don't need it anymore.

--- src/services/stores/talks.js
+++ src/services/stores/talks.js
@@ -29,10 +29,10 @@ export const useTalkStore = defineStore("talks", () => {
                 console.log("Success:", data);
             })
             .catch(() => {
-                // router.push({
-                //     name: "404Resource",
-                //     params: { resource: "chamada criar conversa", redirect: "Home" }
-                // });
+                router.push({
+                    name: "404Resource",
+                    params: { resource: "chamada criar conversa", redirect: "Home" }
+                });
             });
     }
 
@@ -51,16 +51,16 @@ export const useTalkStore = defineStore("talks", () => {
                     data.forEach((talk) => {
                         talks.value = {};
                         data.forEach((talk) => {
-                            talks.value[talk.chat_id] = talk;
+                            talks.value[talk.whatsapp_identification] = talk;
                         });
                     });
                 }
             })
             .catch(() => {
-                // router.push({
-                //     name: "404Resource",
-                //     params: { resource: "chamada encontrar conversa" }
-                // });
+                router.push({
+                    name: "404Resource",
+                    params: { resource: "chamada encontrar conversa" }
+                });
             });
     }
     async function findOneTalkByChatID() {
@@ -76,10 +76,10 @@ export const useTalkStore = defineStore("talks", () => {
                 if (data) activeChat.value = data;
             })
             .catch(() => {
-                // router.push({
-                //     name: "404Resource",
-                //     params: { resource: "chamada encontrar conversa por id" }
-                // });
+                router.push({
+                    name: "404Resource",
+                    params: { resource: "chamada encontrar conversa por id" }
+                });
             });
     }
     async function findAllTalksByUser() {
@@ -96,15 +96,15 @@ export const useTalkStore = defineStore("talks", () => {
                 if (data) {
                     talks.value = {};
                     data.forEach((talk) => {
-                        talks.value[talk.chat_id] = talk;
+                        talks.value[talk.whatsapp_identification] = talk;
                     });
                 }
             })
             .catch(() => {
-                // router.push({
-                //     name: "404Resource",
-                //     params: { resource: "chamada encontrar conversa por usuário" }
-                // });
+                router.push({
+                    name: "404Resource",
+                    params: { resource: "chamada encontrar conversa por usuário" }
+                });
             });
     }
     async function updateTalk(req, res) {}
@@ -122,34 +122,10 @@ export const useTalkStore = defineStore("talks", () => {
             .then((response) => response.json())
             .then((data) => {})
             .catch(() => {
-                // router.push({
-                //     name: "404Resource",
-                //     params: { resource: "chamada atualizar conversa para usuário", redirect: "Home" }
-                // });
-            });
-    }
-    async function updateRobot(assumir) {
-        const last = activeChat.value.length - 1;
-        const url = "https://app.aquicob.com.br/api.php?a=robo&b=transfere_chat";
-        await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            body: JSON.stringify({
-                instance: activeChat.value[last].tlk_robot_instance,
-                chat_id: activeChat.value[last].tlk_chat_id,
-                token: "661F2EC9C06E8A7AC84561CD206196AEE3D3962C35F1148302EDA31E71D37D91",
-                responde: assumir
-            })
-        })
-            .then((response) => response.json())
-            .then((data) => {})
-            .catch(() => {
-                // router.push({
-                //     name: "404Resource",
-                //     params: { resource: "chamada atuaizar robô", redirect: "Home" }
-                // });
+                router.push({
+                    name: "404Resource",
+                    params: { resource: "chamada atualizar conversa para usuário", redirect: "Home" }
+                });
             });
     }
     async function fetchTalks() {
@@ -164,7 +140,6 @@ export const useTalkStore = defineStore("talks", () => {
         findOneTalkByChatID,
         updateTalk,
         updateTalkToSignInUser,
-        updateRobot,
         fetchTalks
     };
 });
