dd28513d017ca4966eceafa10f90080f6c41f3b1
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-25 02:57:00 -0300

    Refactor contact search and display logic in ContactList.vue

--- src/components/side_menu/ContactList.vue
+++ src/components/side_menu/ContactList.vue
@@ -1,54 +1,29 @@
 <script setup>
 import { ref, computed } from "vue";
 import { useTalkStore } from "../../services/stores/talks";
-import { useUsersStore } from "../../services/stores/users";
-import { useFiltersStore } from "../../services/stores/filters";
 import ContactCard from "./ContactCard.vue";
 import Filter from "./Filter.vue";
 
 const emit = defineEmits(["update:modelValue"]);
 const talkStore = useTalkStore();
-const userStore = useUsersStore();
 const search = ref("");
-const filters = useFiltersStore();
 
 const selectTalk = async (talk) => {
 	emit("update:modelValue", "Loading");
-	talkStore.selected = talk.chat_id;
+	talkStore.selected = talk.whatsapp_identification;
 	await talkStore.findOneTalkByChatID().then(emit("update:modelValue", "Chat"));
 };
 
 const searchContact = computed(() => {
 	return Object.values(talkStore.talks).filter((talk) => {
 		if (/^\d+$/.test(search.value.replace(" ", "").replace("-", ""))) {
-			return talk.chat_id.includes(search.value);
+			return talk.whatsapp_identification.includes(search.value);
 		}
-		if (talk.tlk_chat_name == undefined) {
-			return false;
-		}
-		return talk.tlk_chat_name.toLowerCase().includes(search.value.toLowerCase());
+		return talk.whatsapp_identification
 	});
 });
-const filteredContact = computed(() => {
-	return searchContact.value.filter((talk) => {
-		if (filters.selected.includes("showAll")) {
-			return true;
-		}
-		if (filters.selected.includes("onlyMine") && talk.tlk_fk_usu_identification == userStore.user.usu_identification) {
-			return true;
-		}
-		if (filters.selected.includes("waiting") && talk.tlk_fk_frt_identification == 2) {
-			return true;
-		}
-		if (filters.selected.includes("open") && talk.tlk_fk_frt_identification == 1) {
-			return true;
-		}
-		if (filters.selected.includes("closed") && talk.tlk_fk_frt_identification == 4) {
-			return true;
-		}
-		return !!(filters.selected.includes("onlyTheirs") && talk.tlk_fk_usu_identification != userStore.user.usu_identification);
-	});
-});
+
+console.log(searchContact, talkStore.talks);
 </script>
 
 <template>
@@ -57,7 +32,9 @@ const filteredContact = computed(() => {
 			<Filter v-model:search="search" />
 		</div>
 		<div class="overflow-y-auto h-full">
-			<ContactCard v-for="(talk, index) in filteredContact" :key="index" @click="selectTalk(talk)" :talk="talk" />
+			<div v-for="(talk, index) in searchContact">
+				<ContactCard  :key="index" @click="selectTalk(talk)" :talk="talk" />
+			</div>
 		</div>
 	</div>
 </template>
