899d15b1f2e7470eb43242ce9158cf3e918018f6
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-28 01:45:59 -0300

    Add showTasks variable and update SideMenuButtons.vue

--- src/components/side_menu/SideMenuButtons.vue
+++ src/components/side_menu/SideMenuButtons.vue
@@ -15,6 +15,7 @@ const showChat = ref(false);
 const showAnalytics = ref(false);
 const showMenu = ref(false);
 const showMenuLabel = ref(false);
+const showTasks = ref(false);
 
 const handleSignOut = async () => {
 	try {
@@ -44,8 +45,8 @@ function updateStatus(identification) {
 			<div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3" v-show='showMenu'>
 				<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
 					@mouseover="showChat = true" @mouseleave="showChat = false" @click="$emit('update:modelValue', 'Chat')">
-					<svg v-show='!showChat' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" class="w-6 h-6">
+					<svg v-show='!showChat' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+						stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 						<path stroke-linecap="round" stroke-linejoin="round"
 							d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
 					</svg>
@@ -56,8 +57,8 @@ function updateStatus(identification) {
 				<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
 					@mouseover="showAnalytics = true" @mouseleave="showAnalytics = false"
 					@click="$emit('update:modelValue', 'Analytics')">
-					<svg v-show='!showAnalytics' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						stroke="currentColor" class="w-6 h-6">
+					<svg v-show='!showAnalytics' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+						stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 						<path stroke-linecap="round" stroke-linejoin="round"
 							d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
 					</svg>
@@ -67,8 +68,8 @@ function updateStatus(identification) {
 				</div>
 				<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
 					@mouseover="showTeam = true" @mouseleave="showTeam = false" @click="$emit('update:modelValue', 'Team')">
-					<svg v-show='!showTeam' xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24"
-						stroke="currentColor" stroke-width="2">
+					<svg v-show='!showTeam' xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none"
+						viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 						<path stroke-linecap="round" stroke-linejoin="round"
 							d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
 					</svg>
@@ -78,8 +79,8 @@ function updateStatus(identification) {
 				</div>
 				<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base 2xl:col-span-2"
 					@mouseover="showConfig = true" @mouseleave="showConfig = false" @click="configLoggedUser">
-					<svg v-show='!showConfig' xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24"
-						stroke="currentColor" stroke-width="2">
+					<svg v-show='!showConfig' xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none"
+						viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 						<path stroke-linecap="round" stroke-linejoin="round"
 							d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
 						<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -89,9 +90,21 @@ function updateStatus(identification) {
 					</transition>
 				</div>
 				<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
+					@mouseover="showTasks = true" @mouseleave="showTasks = false"
+					@click="$emit('update:modelValue', 'Tasks')">
+					<svg v-show='!showTasks' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+						stroke="currentColor" class="w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round"
+							d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+					</svg>
+					<transition name="slide-fade" :duration="{ enter: 50, leave: 50 }">
+						<h4 v-show="showTasks" class="mx-3 sm:hidden lg:inline">Tarefas</h4>
+					</transition>
+				</div>
+				<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
 					@mouseover="showLoggout = true" @mouseleave="showLoggout = false" @click="handleSignOut">
-					<svg v-show='!showLoggout' xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24"
-						stroke="currentColor" stroke-width="2">
+					<svg v-show='!showLoggout' xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none"
+						viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 						<path stroke-linecap="round" stroke-linejoin="round"
 							d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
 					</svg>
@@ -104,13 +117,12 @@ function updateStatus(identification) {
 
 		<div class="py-7 px-4 flex items bg-indigo-700 hover:bg-indigo-600 text-white text-lg md:text-base"
 			@click="showMenu = !showMenu" @mouseover="showMenuLabel = true" @mouseleave="showMenuLabel = false">
-			<svg v-show='!showMenuLabel' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-				class="w-6 h-6">
+			<svg v-show='!showMenuLabel' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+				stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 			</svg>
 			<transition name="slide-fade" :duration="{ enter: 50, leave: 50 }">
 				<h4 v-show="showMenuLabel" class="mx-3 sm:hidden lg:inline">Menu</h4>
 			</transition>
 		</div>
-	</div>
-</template>
+</div></template>
