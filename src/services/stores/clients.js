8b8d012cd9d87988e9e73cfda53e5a20e601bcc1
Bruno Leria <brunovolpatol@hotmail.com>
2023-09-30 16:46:31 -0300

    Refactor fetch functions to use async/await syntax

--- src/services/stores/clients.js
+++ src/services/stores/clients.js
@@ -20,79 +20,77 @@ export const useClientsStore = defineStore("client", () => {
         return response.status;
     }
 
-    async function findAllClients(req, res) {
+    async function findAllClients() {
         const url = GCurl + "clients";
-        const result = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .catch((error) => {
-                router.push({
-                    name: "404Resource",
-                    params: { resource: "chamada encontrar todas os clientes" }
-                });
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })  
+            return await response.json();  
+        } catch (error) {
+            router.push({
+                name: "404Resource",
+                params: { resource: "chamada encontrar todas os clientes" }
             });
-        return result;
+        }
     }
 
     async function findOneClientById(id) {
         const url = GCurl + "clients/" + id;
-        const retorno = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then((response) => {
-                return response.json();
-            })
-            .catch((error) => {
-                router.push({
-                    name: "404Resource",
-                    params: { resource: "chamada encontrar um cliente" }
-                });
+        try {
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
             });
-        return retorno;
+            return await response.json();  
+        } catch (error) {
+            router.push({
+                name: "404Resource",
+                params: { resource: "chamada encontrar um cliente" }
+            });
+        }
     }
 
     async function updateClient(newClient, identification) {
         const url = GCurl + "clients/" + identification;
-        await fetch(url, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(newClient)
-        })
-            .then((response) => response.json())
-            .catch((error) => {
-                router.push({
-                    name: "404Resource",
-                    params: { resource: "chamada atualizar um cliente" }
-                });
+        try {
+            const response = await fetch(url, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newClient)
             });
+            return await response.status;
+        } catch (error) {
+            router.push({
+                name: "404Resource",
+                params: { resource: "chamada atualizar um cliente" }
+            });
+        }
     }
 
     async function deleteClient(identification) {
         const url = GCurl + "clients/" + identification;
-        await fetch(url, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then((response) => response.json())
-            .catch((error) => {
-                router.push({
-                    name: "404Resource",
-                    params: { resource: "chamada deletar um cliente" }
-                });
+        try {
+            const response = await fetch(url, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                }
             });
+            return await response.status;
+        } catch (error) {
+            router.push({
+                name: "404Resource",
+                params: { resource: "chamada deletar um cliente" }
+            });
+        }
     }
 
     return {
