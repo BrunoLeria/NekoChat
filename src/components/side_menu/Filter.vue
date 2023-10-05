f222127059775768b0bf3c2cf9004c989604e758
Bruno Leria <brunovolpatol@hotmail.com>
2023-10-05 13:27:42 -0300

    Refactor side menu filter input and filter toggle button in Filter.vue

--- src/components/side_menu/Filter.vue
+++ src/components/side_menu/Filter.vue
@@ -12,18 +12,13 @@ const showFilters = ref(false);
 <template>
 	<div class="overflow-visible">
 		<div class="w-full p-5 flex items-center space-x-5">
-			<input
-				type="text"
-				class="w-full rounded-full bg-indigo-400 text-white placeholder:text-white border-white"
-				:value="search"
-				@input="$emit('update:search', $event.target.value)"
-				placeholder="Search" />
+			<input type="text" class="w-full rounded-full bg-indigo-400 text-white placeholder:text-white border-white"
+				:value="search" @input="$emit('update:search', $event.target.value)" placeholder="Search" />
 			<div @click="showFilters = !showFilters">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+					stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
 				</svg>
 			</div>
 		</div>
