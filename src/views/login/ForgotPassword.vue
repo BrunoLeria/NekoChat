878ed7b48f4fc736b941cbeae05d727e48127683
Bruno Leria <brunovolpatol@hotmail.com>
2023-10-05 13:18:46 -0300

    Refactor import paths and update button styling in ForgotPassword.vue

--- src/views/login/ForgotPassword.vue
+++ src/views/login/ForgotPassword.vue
@@ -1,8 +1,7 @@
 <script setup>
-import TextInput from "/src/components/inputs/TextInput.vue";
-import PasswordInput from "/src/components/inputs/PasswordInput.vue";
-import Sublink from "/src/components/buttons/Sublink.vue";
-import logo from "/src/assets/nekologo.jpeg";
+import TextInput from "../../components/inputs/TextInput.vue";
+import Sublink from "../../components/buttons/Sublink.vue";
+import logo from "../../assets/nekologo.jpeg";
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
@@ -43,7 +42,8 @@ function sendEmailPasswordRecovery() {
 </script>
 
 <template>
-	<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" @keypress.enter="sendEmailPasswordRecovery()">
+	<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+		@keypress.enter="sendEmailPasswordRecovery()">
 		<div class="max-w-md w-full space-y-8">
 			<div>
 				<img class="mx-auto h-36 w-auto rounded-full" :src="logo" alt="Workflow" />
@@ -56,15 +56,14 @@ function sendEmailPasswordRecovery() {
 			<form class="mt-8 space-y-6 border-slate-200 border-2 rounded-xl p-5 shadow-xl" action="#" method="POST">
 				<input type="hidden" name="remember" value="true" />
 				<div class="rounded-md shadow-sm -space-y-px">
-					<p class="mt-2 text-center text-sm" :class="sucess ? 'text-green-600' : 'text-red-600'" v-show="infoMsg != ''">
+					<p class="mt-2 text-center text-sm" :class="sucess ? 'text-green-600' : 'text-red-600'"
+						v-show="infoMsg != ''">
 						{{ infoMsg }}
 					</p>
 					<TextInput label="Email" v-model="email" type="email" id="email" autoComplete="email" />
 				</div>
 				<div>
-					<button
-						type="button"
-						class="
+					<button type="button" class="
 							group
 							relative
 							w-full
@@ -82,19 +81,12 @@ function sendEmailPasswordRecovery() {
 							focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
 							ease-in-out
 							duration-500
-						"
-						@click="sendEmailPasswordRecovery">
+						" @click="sendEmailPasswordRecovery">
 						<span class="absolute left-0 inset-y-0 flex items-center pl-3">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="lock-closed h-5 w-5 text-blue-500 group-hover:text-blue-400"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-								stroke-width="2">
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
+							<svg xmlns="http://www.w3.org/2000/svg"
+								class="lock-closed h-5 w-5 text-blue-500 group-hover:text-blue-400" fill="none"
+								viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+								<path stroke-linecap="round" stroke-linejoin="round"
 									d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
 							</svg>
 						</span>
