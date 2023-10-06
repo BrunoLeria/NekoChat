10e942c7f272deec195f595f0484ae1507336511
Bruno Leria <brunovolpatol@hotmail.com>
2023-10-06 03:08:04 -0300

    Refactor phone masking function and update client rendering logic in Clients.vue

--- src/views/home/Clients.vue
+++ src/views/home/Clients.vue
@@ -1,30 +1,24 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
 import ClientCard from "../../components/cards/ClientCard.vue";
 import { useClientsStore } from "../../services/stores/clients";
+import { formatPhoneNumber } from "../../utils/DataTreament";
 
 const emit = defineEmits(["update:modelValue"]);
 const clientsStore = useClientsStore();
-const clients = ref([]);
-
-onMounted(async () => {
-    clients.value = await clientsStore.findAllClients();
-});
 
 const newTask = () => {
     window.open("client", "Ratting", "width=700, height = 640, left = 480, top = 200, toolbar = 0, status = 0, ");
 };
 
 function maskedPhone(phone) {
-    const phoneWithoutPrefix = phone.slice(2);
-    return phoneWithoutPrefix.replace(/\D/g, "").replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    return formatPhoneNumber(phone);
 };
 
 </script>
 
 <template>
     <div class="bg-neutral-100 p-14 grid gap-5 grid-rows-6 grid-cols-3 row-span-6">
-        <div v-for='client in clients'>
+        <div v-for='client in clientsStore.clients'>
             <ClientCard :key='client.identification' :identification='client.identification' :name='client.name'
                 :email='client.email' :phone="maskedPhone(client.phone)" />
         </div>
@@ -52,9 +46,8 @@ function maskedPhone(phone) {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                        d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
                 </svg>
-
             </button>
         </div>
     </div>
