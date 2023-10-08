977a5230deb14a88119e6a1f82de9ec4d5627584
Bruno Leria <brunovolpatol@hotmail.com>
2023-10-08 05:02:28 -0300

    Refactor Team.vue to display either a list of users or a list of teams based on user selection

--- src/views/home/Team.vue
+++ src/views/home/Team.vue
@@ -4,15 +4,15 @@ import { useUsersStore } from "../../services/stores/users";
 import { useStatusesStore } from "../../services/stores/status";
 import { onBeforeMount, ref } from 'vue';
 import UserCard from "../../components/cards/UserCard.vue";
+import TeamCard from "../../components/cards/TeamCard.vue";
 
 const teamStore = useTeamStore();
 const userStore = useUsersStore();
 const emit = defineEmits(["update:modelValue"]);
 const statuses = useStatusesStore().statuses;
 const users = ref([]);
-const teams = useTeamStore().teams;
-
-
+const teams = teamStore.teams;
+const membersView = ref(true);
 
 onBeforeMount(async () => {
 	users.value = await userStore.findAllUsers();
@@ -20,7 +20,24 @@ onBeforeMount(async () => {
 
 </script>
 <template>
-	<div class="bg-neutral-100 p-14 grid grid-cols-3 gap-5">
-		<UserCard v-for="user in users" :key="user.identification" :member="user" :teams='teams' :statuses='statuses' />
+	<div class="bg-neutral-300 flex flex-col p-14">
+		<div class='grid grid-cols-2'>
+			<div class='flex h-12 border-2 border-gray-200 rounded-tl-2xl hover:border-b-2 hover:shadow-indigo-500 hover:border-indigo-500 shadow-lg ease-in-out duration-500 items-center justify-center'
+				:class='membersView ? "bg-white border-b-0" : "bg-neutral-100 border-b-2"' @click='membersView = true'>
+				<p>Participantes</p>
+			</div>
+			<div class='flex h-12 border-2 border-gray-200 rounded-tr-2xl hover:border-b-2 hover:shadow-indigo-500 hover:border-indigo-500 shadow-lgease-in-out duration-500 items-center justify-center'
+				:class='!membersView ? "bg-white border-b-0" : "bg-neutral-100 border-b-2"' @click='membersView = false'>
+				<p>Equipes</p>
+			</div>
+		</div>
+		<div v-if='membersView'
+			class="bg-white p-8 border-2 border-t-0 border-gray-200 row-span-5 col-span-2 grid grid-rows-6 grid-cols-2 xl:grid-cols-3 gap-x-3.5 gap-y-32 h-full">
+			<UserCard v-for="user in users" :key="user.identification" :member="user" :teams='teams' :statuses='statuses' />
+		</div>
+		<div v-else
+			class="bg-white p-8 border-2 border-t-0 border-gray-200 row-span-5 col-span-2 grid grid-rows-6 grid-cols-2 xl:grid-cols-3 gap-x-3.5 gap-y-32 h-full ">
+			<TeamCard v-for="team in teams" :key="team.identification" :team="team" />
+		</div>
 	</div>
 </template>
